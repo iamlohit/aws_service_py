2b3d63511b8432e2905ccee90ee56a472cb83ef2
Lohit Jethwani <jethwanilohit@gmail.com>
2025-06-04 17:22:09 +0530

    Starting with RDS boto3 client

---
+++ rds_lj.py
@@ -0,0 +1,25 @@
+import boto3
+import time
+
+# Instantiate a boto3 client for RDS, Note: There is no Resource method for RDS, only client.
+
+
+# User Defined Variables
+
+
+# db_subnet_group
+
+
+# db_cluster_id
+
+
+# Create the DB Cluster
+
+
+# Wait for DB cluster to become available
+
+
+# Modify the DB cluster. Update the scaling config for the cluster.
+
+
+# Delete the DB Cluster
